d33f231926bc67e07e8bfc067d3d982d6ef7aaae
calcuis <113646141+calcuis@users.noreply.github.com>
2023-09-18 08:57:37 +0800

    Update csv_json.py

--- csv_json.py
+++ csv_json.py
@@ -1,18 +1,12 @@
 import csv, json
 
-def csv_to_json(csvFilePath, jsonFilePath):
-    data = []
+data = []
 
-    with open(csvFilePath, encoding='utf-8-sig') as csvf:
-        csvReader = csv.DictReader(csvf) 
+with open('data.csv', encoding='utf-8-sig') as csvf:
+    csvReader = csv.DictReader(csvf)
 
-        for row in csvReader: 
-            data.append(row)
+    for row in csvReader: 
+        data.append(row)
 
-    with open(jsonFilePath, 'w', encoding='utf-8-sig') as jsonf:
-        jsonf.write(json.dumps(data, indent=4))
-
-csvFilePath = r'data.csv'
-jsonFilePath = r'data.json'
-
-csv_to_json(csvFilePath, jsonFilePath)
+with open('data.json', 'w', encoding='utf-8-sig') as jsonf:
+    jsonf.write(json.dumps(data, indent=4))
